dee16be80db6f39938d35e16ce245465e95e62e8
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-05-31 14:19:27 +0600

    developed: welcome section for dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/db";
-import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -16,8 +16,26 @@ const Dashboard = async () => {
 
   if (!userSettings) redirect("/wizerd");
   return (
-    <div>
-      <UserButton />
+    <div className="h-full bg-background">
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <p className="text-3xl font-bold">Hello, {user.firstName}! 🤚</p>
+          <div className="flex items-center gap-3">
+            <Button
+              variant="outline"
+              className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+            >
+              New income 🤑
+            </Button>
+            <Button
+              variant="outline"
+              className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
+            >
+              New expense 😤
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
