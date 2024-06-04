b480e09d16a82e6100b7d5c735697f19f52002dc
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-06-04 15:13:08 +0600

    refactor: dynamic imports

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,54 +1,10 @@
-"use client";
-import { DateRangePicker } from "@/components/ui/date-range-picker";
-import { MAX_DATE_RANGE_DAYS } from "@/lib/constants";
-import { differenceInDays, startOfMonth } from "date-fns";
-import { useState } from "react";
-import { toast } from "sonner";
-import TransactionTable from "./_components/transaction-table";
+import dynamic from "next/dynamic";
+const TransactionTableContainer = dynamic(
+  () => import("./_components/transaction-table-container")
+);
 
-const TransactionPage = () => {
-  const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
-    from: startOfMonth(new Date()),
-    to: new Date(),
-  });
-
-  return (
-    <>
-      <div className="border-b bg-card">
-        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-          <div>
-            <p className="text-3xl font-bold">Transaction history</p>
-          </div>
-          <DateRangePicker
-            initialDateFrom={dateRange.from}
-            initialDateTo={dateRange.to}
-            showCompare={false}
-            onUpdate={(values) => {
-              const { from, to } = values.range;
-
-              // we update the date range only if both dates are set
-
-              if (!from || !to) {
-                return;
-              }
-
-              if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
-                toast.error(
-                  `The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days`
-                );
-                return;
-              }
-
-              setDateRange({ from, to });
-            }}
-          />
-        </div>
-      </div>
-      <div className="container">
-        <TransactionTable from={dateRange.from} to={dateRange.to} />
-      </div>
-    </>
-  );
+const Page = () => {
+  return <TransactionTableContainer />;
 };
 
-export default TransactionPage;
+export default Page;
