dd00e45cbc892837cf3b1d3ea925b7c45da7d20d
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-06-15 14:35:29 +0600

    removed: runtime edge

--- src/app/api/bank/route.ts
+++ src/app/api/bank/route.ts
@@ -2,9 +2,6 @@ import prisma from "@/lib/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-// Export the runtime config to specify that this route should be handled by the edge
-export const runtime = "edge";
-
 export async function GET(req: Request, res: Response) {
   const user = await currentUser();
 
--- src/app/api/transactions-history/route.ts
+++ src/app/api/transactions-history/route.ts
@@ -4,9 +4,6 @@ import { overviewQuerySchema } from "@/schema/overview";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-// Export the runtime config to specify that this route should be handled by the edge
-export const runtime = "edge";
-
 export async function GET(req: Request) {
   const user = await currentUser();
 
